d0014890b2f9ad5dfe18570dddb61b4b76309ebd
falkowa42 <falkowa42@gmail.com>
2021-08-05 08:00:17 -0400

    made changes to environment variables for more security

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  API_KEY: 'AIzaSyB3L1ooJmp5Ix_yYuYZyzXd4RqGycPnnm0'
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
-  API_KEY: 'AIzaSyB3L1ooJmp5Ix_yYuYZyzXd4RqGycPnnm0'
+  production: false
 };
