52a59f970326293cfa783154892e112685eb458a
falkowa42 <falkowa42@gmail.com>
2021-08-13 06:29:38 -0400

    added code for updating the state within the application using ngrx

--- src/app/shoppingList/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shoppingList/shopping-list/store/shopping-list.reducer.ts
@@ -1,27 +1,78 @@
 import {Ingredient} from "../../../shared/ingredient.model";
 import * as ShoppingListActions from "./shopping-list.actions";
 
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number
+}
 
-const initialState = {
+export interface AppState {
+  shoppingList: State;
+}
+
+const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ]
+    new Ingredient('Tomatoes', 10)
+  ], editedIngredient: null,
+  editedIngredientIndex: -1
 };
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+export function shoppingListReducer(state: State = initialState, action: ShoppingListActions.ShoppingListActions) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
-    case ShoppingListActions.ADD_INGREDIENTS:{
+    case ShoppingListActions.ADD_INGREDIENTS: {
       return {
         ...state,
-        ingredients: [...state.ingredients,...action.payload]
+        ingredients: [...state.ingredients, ...action.payload]
       }
     }
+    case ShoppingListActions.UPDATE_INGREDIENT: {
+      // finds the ingredient from the initial state
+      const ingredient = state.ingredients[state.editedIngredientIndex];
+      // creates a copy of the old ingredient, overwrite with new ingredient passed
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload
+      }
+      // creates new array of ingredients
+      const updatedIngredients = [...state.ingredients];
+      //finds the ingredient that needs to be updated, and updates it with the new ingredient
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+      // returns the new ingredient array with the update
+      return {
+        ...state,
+        ingredients: updatedIngredients,
+        editedIngredientIndex: -1,
+        editedIngredient: null
+      }
+    }
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ig, index) => {
+          return index !== state.editedIngredientIndex;
+        }),
+        editedIngredientIndex: -1,
+        editedIngredient: null
+      }
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: {...state.ingredients[action.payload]}
+      }
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      }
     default:
       return state;
   }
