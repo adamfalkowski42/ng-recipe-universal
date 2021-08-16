b5904b0326a604eca5de4d67cf53a9177897d354
falkowa42 <falkowa42@gmail.com>
2021-08-16 17:03:03 -0400

    made more changes to other components using ngrx

--- src/app/shoppingList/shopping-list/shopping-list.component.ts
+++ src/app/shoppingList/shopping-list/shopping-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
-import {ShoppingListService} from './shopping-list.service';
+
 import {Observable, Subscription} from "rxjs";
 import {LoggingService} from 'src/app/logging.service';
 import {Store} from "@ngrx/store";
 import * as fromShoppingList from "./store/shopping-list.reducer";
 import * as ShoppingListActions from "./store/shopping-list.actions";
-import {StartEdit} from "./store/shopping-list.actions";
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,7 +17,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ingredients: Ingredient[]}>
   private subscription: Subscription;
 
-  constructor(private shoppinglistService: ShoppingListService, private loggingService: LoggingService, private store: Store<fromShoppingList.AppState>) {
+  constructor(private loggingService: LoggingService, private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit(): void {
