0bed61cba67283f646787ff3ad6ca087584a76d2
falkowa42 <falkowa42@gmail.com>
2021-08-22 10:16:24 -0400

    added in angular universal code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Inject, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common'
 import {AuthService} from "./auth/auth.service";
 import {LoggingService} from "./logging.service";
 import * as fromApp from './store/app.reducer';
@@ -13,10 +14,15 @@ import {AutoLogin} from "./auth/store/auth.actions";
 })
 export class AppComponent implements OnInit{
 
-  constructor(private authService: AuthService, private loggingService: LoggingService,  private store: Store<fromApp.AppState> ){}
+  constructor(private authService: AuthService,
+              private loggingService: LoggingService,
+              private store: Store<fromApp.AppState>,
+              @Inject(PLATFORM_ID)private platformId ){}
   ngOnInit() {
     // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if(isPlatformBrowser(this.platformId)){
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello AppComponent ngOnit');
   }
 
